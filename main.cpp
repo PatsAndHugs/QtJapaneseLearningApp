54e08261f17497b67c920961fa812fdd27f94138
P&H <jocast.apps@gmail.com>
2025-09-01 14:25:09 +0800

    changed start window to main

--- main.cpp
+++ main.cpp
@@ -25,7 +25,7 @@ int main(int argc, char *argv[])
         Qt::QueuedConnection);
 
     engine.rootContext()->setContextProperty(QStringLiteral("kanjiList"), &kanjiList);
-    engine.loadFromModule("QtJapaneseLearningApp", "KanjiTestWindow");
+    engine.loadFromModule("QtJapaneseLearningApp", "Main");
 
     return app.exec();
 }
